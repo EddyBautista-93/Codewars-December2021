e6cfb650367a93fec35bcbc2746e17900aaa65c8
EddyBautista <bautista.j.eddy@gmail.com>
2021-12-01 03:35:17 -0600

    deaf rat of hamelin set up

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Codewars_December2021
+{
+    class Program
+    {
+        // The deaf rats of Hamelin
+        // The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+
+        // But some of the rats are deaf and are going the wrong way!
+
+        // Kata Task
+        // How many deaf rats are there?
+
+        // Legend
+        // P = The Pied Piper
+        // O~ = Rat going left
+        // ~O = Rat going right
+
+        public static int CountDeafRats(string town)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            CountDeafRats("~O~O~O~O P");
+            
+        }
+    }
+}
